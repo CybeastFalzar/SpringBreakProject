c9e559aa49f83a71e47f4dfc50edae1918877056
CybeastFalzar <videshncollege@gmail.com>
2022-04-17 21:08:03 -0400

    Reload Sales tables

---
+++ Tables/DataTransfer.Sales2017.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [DataTransfer].[Sales2017]
+(
+[MakeName] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ModelName] [nvarchar] (150) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CustomerName] [nvarchar] (150) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CountryName] [nvarchar] (150) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Cost] [money] NULL,
+[RepairsCost] [money] NULL,
+[PartsCost] [money] NULL,
+[TransportInCost] [money] NULL,
+[SalePrice] [numeric] (18, 2) NULL,
+[SaleDate] [datetime] NULL
+) ON [PRIMARY]
+GO
