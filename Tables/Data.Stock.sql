231b8ebda7c3f1cba6a621eb0a5a62f377793b39
CybeastFalzar <videshncollege@gmail.com>
2022-04-29 17:14:28 -0400

    Finish Updating UDT's Need to Still finissh Time, Date, and Price to updated values

--- Tables/Data.Stock.sql
+++ Tables/Data.Stock.sql
@@ -1,17 +1,14 @@
 CREATE TABLE [Data].[Stock]
 (
-[StockCode] [Udt].[Stock] NOT NULL CONSTRAINT [DF_Stock_StockCode] DEFAULT (newid()),
-[ModelID] [smallint] NULL,
-[Cost] [money] NULL,
-[RepairsCost] [money] NULL,
-[PartsCost] [money] NULL,
-[TransportInCost] [money] NULL,
-[IsRHD] [bit] NULL,
-[Color] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[BuyerComments] [nvarchar] (4000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[StockCode] [Udt].[Stock] NULL CONSTRAINT [DF_Stock_StockCode] DEFAULT (newid()),
+[ModelID] [Udt].[ID] NULL,
+[Cost] [Udt].[Price] NULL,
+[RepairsCost] [Udt].[Price] NULL,
+[PartsCost] [Udt].[Price] NULL,
+[TransportInCost] [Udt].[Price] NULL,
+[IsRHD] [Udt].[BinaryChoice] NULL,
+[Color] [Udt].[Name] NULL,
 [DateBought] [date] NULL,
 [TimeBought] [time] NULL
 ) ON [PRIMARY]
-GO
-ALTER TABLE [Data].[Stock] ADD CONSTRAINT [PK_Stock] PRIMARY KEY CLUSTERED ([StockCode]) ON [PRIMARY]
 GO
